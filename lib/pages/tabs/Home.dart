853b2ca313943549863dc05f1b8e9cb9fd576a17
15635389119 <434317753@qq.com>
2020-07-24 11:51:21 +0800

    Further improve the home page.

--- lib/pages/tabs/Home.dart
+++ lib/pages/tabs/Home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:sunnytravel_app/components/ImageButton.dart';
 
 class SwiperPage extends StatefulWidget {
   SwiperPage({Key key}) : super(key: key);
@@ -44,6 +45,114 @@ class _SwiperPageState extends State<SwiperPage> {
                 //control: new SwiperControl(),
               ),
               )
+            ),
+            SizedBox(
+              height: 10.0,
+              child:  Container(
+                height: 10.0,
+                color: Color(0xffffffff),
+              ),
+            ),
+            Row(
+              children: <Widget>[
+                 ImageButton(
+                  children: <Widget>[],
+                  width: 200,
+                  height: 140,
+                  paddingTop: 5,
+                  title: 'Lijiang',
+                  normalStyle: TextStyle(color: Colors.black, fontSize: 14, decoration: TextDecoration.none),
+                  pressedImage: Image.asset(
+                    "images/lj.jpg",
+                  ),
+                  unpressedImage: Image.asset(
+                    "images/lj.jpg"
+                  ),
+                  onTap: () {
+                    print('test');
+                  },
+                ),
+                SizedBox(
+                  width: 11.3,
+                  height: 140.0,
+                  child:  Container(
+                    width: 11.0,
+                    height: 160.0,
+                    color: Color(0xffffffff),
+                  ),
+                ),
+                ImageButton(
+                  children: <Widget>[],
+                  width: 200,
+                  height: 140,
+                  paddingTop: 5,
+                  title: 'Zhangjiajie',
+                  normalStyle: TextStyle(color: Colors.black, fontSize: 14, decoration: TextDecoration.none),
+                  pressedImage: Image.asset(
+                    "images/zjj.jpg",
+                  ),
+                  unpressedImage: Image.asset(
+                    "images/zjj.jpg"
+                  ),
+                  onTap: () {
+                    print('test');
+                  },
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10.0,
+              child:  Container(
+                height: 10.0,
+                color: Color(0xffffffff),
+              ),
+            ),
+            Row(
+              children: <Widget>[
+                 ImageButton(
+                  children: <Widget>[],
+                  width: 200,
+                  height: 140,
+                  paddingTop: 5,
+                  title: 'Lasa',
+                  normalStyle: TextStyle(color: Colors.black, fontSize: 14, decoration: TextDecoration.none),
+                  pressedImage: Image.asset(
+                    "images/ls.jpg",
+                  ),
+                  unpressedImage: Image.asset(
+                    "images/ls.jpg"
+                  ),
+                  onTap: () {
+                    print('test');
+                  },
+                ),
+                SizedBox(
+                  width: 11.3,
+                  height: 140.0,
+                  child:  Container(
+                    width: 11.0,
+                    height: 160.0,
+                    color: Color(0xffffffff),
+                  ),
+                ),
+                ImageButton(
+                  children: <Widget>[],
+                  width: 200,
+                  height: 140,
+                  paddingTop: 5,
+                  title: 'Guilin',
+                  normalStyle: TextStyle(color: Colors.black, fontSize: 14, decoration: TextDecoration.none),
+                  pressedImage: Image.asset(
+                    "images/gl.jpg",
+                  ),
+                  unpressedImage: Image.asset(
+                    "images/gl.jpg"
+                  ),
+                  onTap: () {
+                    print('test');
+                  },
+                ),
+              ],
             ),
           ],
         ));
